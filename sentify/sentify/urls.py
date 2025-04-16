d72f912df70c3aad3db537e7c4a443ce5668d2c5
Taniya2004 <taniyakanojiya52004@gmail.com>
2025-04-16 18:20:46 +0530

    logo error solved

--- sentify/sentify/urls.py
+++ sentify/sentify/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chat.urls')),
     path('', include('chat.urls_ajax')),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-]
-
+'''
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+'''
