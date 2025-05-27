0b778adb7d3cce6653d4ef4bb4b6ff1bb0112117
Taniya2004 <taniyakanojiya52004@gmail.com>
2025-05-27 13:44:19 +0530

    Database changes done

--- sentify/sentify/settings.py
+++ sentify/sentify/settings.py
@@ -57,11 +57,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'sentify.wsgi.application'
 
 # Database
-# Using default SQLite database (SQL based)
+# Changed from SQLite to MySQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MYSQL',
+        'USER': 'Taniya',
+        'PASSWORD': 'Anjana2004@',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
